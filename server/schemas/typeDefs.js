12029650433a9195444a4869a45fef87b6d8c549
unknown <jozelinesuarez@gmail.com>
2024-01-21 16:46:43 -0600

    back-end: resolvers, typedefs, apollo server, auth

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const typeDefs = `
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }    
+    type Query {
+        me: User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook( SavedBookInput ): User
+        removeBook(bookId: String!): User
+    }
+`
+
+module.exports = typeDefs;
